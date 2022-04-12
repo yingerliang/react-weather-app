f98c9564c078af235e37b2d3cc2c8ddac964501f
yinger <75009049+yingerliang@users.noreply.github.com>
2022-04-12 18:39:35 +1200

    Added weather base

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css'
+import Weather from './Weather'
 
 export default function App() {
   return (
     <div className="App">
-      <div class="container">Weather React App</div>
+      <div className="container">
+        <Weather />
+      </div>
     </div>
   )
 }
---
+++ src/Weather.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import './Weather.css'
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>Auckland</h1>
+      <ul>
+        <li>Tuesday 6:00</li>
+        <li>Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
+            alt="Cloudy"
+          />
+          19°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 72%</li>
+            <li>Wind: 13km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
