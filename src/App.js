cd76f12024e0a40c40fcfb8cb7d89d2119cb5668
yinger <75009049+yingerliang@users.noreply.github.com>
2022-04-12 21:47:53 +1200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ export default function App() {
           <a
             href="https://github.com/yingerliang/vanilla-weather-app"
             target="_blank"
+            rel="noreferrer"
           >
             open-souced.
           </a>
