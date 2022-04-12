12b7b1b5181dcf7008f6329f3693ab1a448787aa
yinger <75009049+yingerliang@users.noreply.github.com>
2022-04-12 17:58:49 +1200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css'
 
 export default function App() {
-  return <div className="App">Weather React App</div>
+  return (
+    <div className="App">
+      <div class="container">Weather React App</div>
+    </div>
+  )
 }
